8c7b2610f642aa5b703f07f6231f71d64b58daf4
kiljoonho <kiljh@me.com>
2019-09-07 23:40:01 +0900

    xib와 함께 뷰컨트롤러 생성

---
+++ PopupModal/modalViewController.swift
@@ -0,0 +1,30 @@
+//
+//  modalViewController.swift
+//  PopupModal
+//
+//  Created by joon-ho kil on 9/7/19.
+//  Copyright © 2019 길준호. All rights reserved.
+//
+
+import UIKit
+
+class modalViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
